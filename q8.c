c6dba8cefe84f8c93812b544b8df241c1b4d726d
Bazil Uddin Khan <rrayyantech@gmail.com>
2026-09-06 15:26:20 +0500

    Create q8.c

---
+++ q8.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int roll_number;
+    printf("Please enter your roll number: ");
+    scanf("%d", &roll_number);
+    printf("Your roll number is %d\n", roll_number);
+    return 0;
+}
