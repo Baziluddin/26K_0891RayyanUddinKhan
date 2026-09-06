d440eb586e18deb2774660515c95edd06d99da4a
Bazil Uddin Khan <rrayyantech@gmail.com>
2026-09-06 15:25:12 +0500

    Create q6.c

---
+++ q6.c
@@ -0,0 +1,10 @@
+C Code
+#include <stdio.h>
+int main()
+{
+    int number = 10;
+    float item_price = 20.50;
+    printf("Number: %d, Price: %.2f\n",
+           number, item_price);
+    return 0;
+}
